19b1fe2502f29f69908311d62bbd35d530374036
Nicolae Grigoriu <ng@inlead.dk>
2018-08-09 12:17:44 +0300

    EASYOPAC-921 - Remove unnecessary method.

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -4,19 +4,9 @@ namespace BTJ\Scrapper\Container;
 
 
 abstract class Container {
-  private $container = NULL;
-
   private $title;
 
   private $body;
-
-  public function getContainer() {
-    if ($this->container === NULL) {
-      $this->container = new Client();
-    }
-
-    return $this->container;
-  }
 
   public function setTitle($title) {
     $this->title = $title;
